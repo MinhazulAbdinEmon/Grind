ae0ee19778e4ee7c451af9da65b01af575e9132c
Minhazul Abdin Emon <165420980+MinhazulAbdinEmon@users.noreply.github.com>
2025-04-12 07:25:56 +0600

    Add files via upload

---
+++ cf_d3_739A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define bl '\n'
+#define ll long long
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int cnt =0;
+        int x;
+        cin>>x;
+        for(int i =1; ;i++){
+            if(i%3 ==0 || i%10 == 3){
+                continue;
+            }
+            cnt++;
+            if(cnt == x){
+                cout<<i<<bl;
+                break;
+            }
+        }
+        
+        
+    }
+}
