001d35163161fc9be21e0973eac7de2ca3ab0a67
Minhazul Abdin Emon <165420980+MinhazulAbdinEmon@users.noreply.github.com>
2025-05-02 22:26:16 +0600

    Update TemplateNoJutsu.cpp

--- TemplateNoJutsu.cpp
+++ TemplateNoJutsu.cpp
@@ -8,87 +8,77 @@ using vi = vector<int>;
 using vll = vector<long long>;
 #define pb push_back
 
-
-
 #ifndef ONLINE_JUDGE
-    #define ryoikiTenkai() freopen("input.txt", "r", stdin); freopen("output.txt", "w",stdout); ios::sync_with_stdio(false); cin.tie(nullptr);
+#define ryoikiTenkai() freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout); ios::sync_with_stdio(false); cin.tie(nullptr);
 #else
-    #define ryoikiTenkai() ios::sync_with_stdio(false); cin.tie(nullptr);
+#define ryoikiTenkai() ios::sync_with_stdio(false); cin.tie(nullptr);
 #endif
 
 template <typename T>
-void print1D(const vector<T>& arr){
-    for(const auto& elem: arr){
-        cout<<elem<<" ";
-    }
-    cout<<bl;
-
+void print1D(const vector<T>& arr) {
+    for (const auto& elem : arr) cout << elem << " ";
+    cout << bl;
 }
 
 template<typename T>
-void print2D(const vector<vector<T>>& mat){
-    for(const auto& row: mat){
-        for(const auto& elem: row){
-            cout<<elem<<" ";
-        }
-        cout<<bl;
-    }
+void print2D(const vector<vector<T>>& mat) {
+    for (const auto& row : mat) print1D(row);
 }
+
 template <typename T>
 vector<vector<T>> make2D(int n,int m, T val){
     return vector<vector<T>>(n,vector<T>(m,val));
 }
+
 template <typename T>
 vector<T> summon1D(int size){
     vector<T>arr(size);
-    for(int i=0;i<size;i++){
-        cin>>arr[i];
-    }
+    for(int i=0;i<size;i++) cin>>arr[i];
     return arr;
 }
 
 template <typename T>
 vector<vector<T>> summon2D(int n,int m){
-
     auto grid = make2D(n,m,T());
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
+    for(int i=0;i<n;i++)
+        for(int j=0;j<m;j++)
             cin>> grid[i][j];
-        }
-    }
     return grid;
 }
 
-//void ryoikiTenkai(){
-//    ios::sync_with_stdio(false);
-//    cin.tie(nullptr);
-//      cout.tie(nullptr);
-//}
 void teleport(int &a, int &b){
-    if(a!=b){ 
-        a = a^b;
-        b = a^b;
-        a = a^b;
+    if(a!=b){
+        a = a^b; b = a^b; a = a^b;
     }
 }
 
 bool pow_of_two(ll a){
-
-    if(a==0)return 0;
-    else if((a&(a-1))==0){ // all zeros(00000...0)
-        return 1;
+    return a && ((a & (a - 1)) == 0);
+}
+int bitBlade(ll x){
+    int cnt=0;
+    while(x){
+        x = x&(x-1);
+        cnt++;
     }
-    return 0;
+    return cnt;
+}
 
+
+
+
+void solve(){
+     
+    
+
+    
 }
 
 int main(){
     ryoikiTenkai();
-    int t;
-    cin>>t;
+    int t =1;
+    // cin>>t;
     while(t--){
-        
-
+        solve();
     }
-}
-
+} 
