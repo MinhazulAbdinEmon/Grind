e4687ccdf4408740f9ede6c22d42df5de5d2ffd4
Minhazul Abdin Emon <165420980+MinhazulAbdinEmon@users.noreply.github.com>
2025-12-09 00:40:48 +0600

    Update and rename Adjacency_Matrix2.cpp to Directed_Unweighted_Graph.cpp

---
+++ Graph/Directed_Unweighted_Graph.cpp
@@ -0,0 +1,23 @@
+//Directed Unweighted Graph
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int vertex,edges;
+    cin>>vertex>>edges;
+    vector<vector<int>>AdjMat(vertex,vector<int>(vertex,0));
+   
+    for(int i=0;i<edges;i++){
+        int u,v;
+        cin>>u>>v;
+        AdjMat[u][v] = 1;
+        
+
+    }
+    
+    for(int i=0;i<vertex;i++){
+        for(int j=0;j<vertex;j++){
+            cout<<AdjMat[i][j]<<" ";
+        }
+        cout<<'\n';
+    }
+}
