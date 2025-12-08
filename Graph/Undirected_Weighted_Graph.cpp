6fc7e96ff085f4306b565949eebcc23ac07f6563
Minhazul Abdin Emon <165420980+MinhazulAbdinEmon@users.noreply.github.com>
2025-12-09 00:41:20 +0600

    Add files via upload

---
+++ Graph/Undirected_Weighted_Graph.cpp
@@ -0,0 +1,22 @@
+// UnDirected Weighted Graph
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int vertex,edges;
+    cin>>vertex>>edges;
+    vector<vector<int>>AdjMat(vertex,vector<int>(vertex,0));
+    for(int i=0;i<edges;i++){
+        int u,v,w;
+        cin>>u>>v>>w;
+        AdjMat[u][v] = w;
+        AdjMat[v][u] = w;
+
+    }
+    // printing the graph
+    for(int i=0;i<vertex;i++){
+        for(int j=0;j<vertex;j++){
+            cout<<AdjMat[i][j]<<" ";
+        }
+        cout<<'\n';
+    }
+}
